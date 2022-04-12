84b5ae25869cc9a5f21bad514c1733deed6f2ef2
jessamynsmith <geekchick77@gmail.com>
2022-04-12 16:51:02 -0600

    Added volley. Added code to call api when text is typed in autocomplete. Does not yet populate autocomplete with new text.

--- app/src/main/java/com/example/myexample/MainActivity.java
+++ app/src/main/java/com/example/myexample/MainActivity.java
@@ -2,28 +2,97 @@ package com.example.myexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class MainActivity extends AppCompatActivity {
-    String[] fruits = {"Apple", "Banana", "Cherry", "Date",
-            "Grape", "Kiwi", "Mango", "Melon", "Pear"};
+    private RequestQueue mRequestQueue;
+    private JsonArrayRequest mRequest;
+    private String authorUrl = "https://underquoted.herokuapp.com/api/v2/author_summary/";
+
+    ArrayAdapter<String> adapter;
+
+    List<String> suggestions = new ArrayList<>();
+
+    private void retrieveData(String searchParam){
+        mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        String url = authorUrl + "?name=" + searchParam;
+        mRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("response", response.toString());
+                        ArrayList<String> newSuggestions = new ArrayList<>();
+                        try {
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject item = response.getJSONObject(i);
+                                newSuggestions.add((String)item.get("name"));
+                            }
+                        } catch (org.json.JSONException e) {
+                            Log.e("JSON exception", e.toString());
+                        }
+                        suggestions = newSuggestions;
+                        Log.d("new suggestions", suggestions.toString());
+                        adapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("JsonArrayRequest error", error.toString());
+
+                    }
+                });
+
+        mRequestQueue.add(mRequest);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Creating the instance of ArrayAdapter containing list of fruit names
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>
-                (this, android.R.layout.select_dialog_item, fruits);
-        //Getting the instance of AutoCompleteTextView
-        AutoCompleteTextView actv = (AutoCompleteTextView) findViewById(R.id.autoComplete);
-        actv.setThreshold(1);//will start working from first character
-        actv.setAdapter(adapter);//setting the adapter data into the AutoCompleteTextView
-        actv.setTextColor(Color.RED);
+        AutoCompleteTextView autocomplete = (AutoCompleteTextView)findViewById(R.id.autoComplete);
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, suggestions);
+        autocomplete.setThreshold(1);
+        autocomplete.setAdapter(adapter);
 
+        autocomplete.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Log.d("afterTextChanged", editable.toString());
+                retrieveData(editable.toString());
+            }
+        });
     }
+
 }
